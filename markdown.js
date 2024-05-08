030a804a07369eca6935c0ce5e1c11942095ee71
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-05-08 11:18:15 +0530

    Markdown-Crash-Course

---
+++ markdown.js
@@ -0,0 +1,43 @@
+// --> What is Markdown ?
+
+/*
+    * Lightweight markup language with a olain text formatting syntax
+    * can be converted into HTML/XHTML and other formats
+    * it's main purpose is readability and ease of use
+     
+    */   
+
+
+// --> What Is It Used For?
+
+/*
+    * Readme Files (Github, etc)
+    * Forum & Blog Posts
+    * Used in many static site generators
+    
+    */
+
+ 
+
+// --> Some Things You Can Format
+
+/*
+    - Headings     - Blocks of code
+    - Lists        - Images
+    - Emphasis     - Blockquotes
+    - Links        - Horizontal Rules
+
+    */    
+
+
+
+// --> Markdown Editors
+
+/*
+    - Text Editor Extensions (VSCode, Atom, etc)
+    - MarkPad
+    - HarooPad
+    - MarkdownPad 2
+    - Typora
+
+    */
